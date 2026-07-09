08d279502406591efd2534b9c63271e6b5a31ccf
parth7830 <parthkartik125@gmail.com>
2026-07-09 12:20:28 +0530

    Time: 1 ms (98.81%), Space: 46.4 MB (49.56%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -1,31 +1,36 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    HashMap<Integer, Integer> inMap = new HashMap<>();
+
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return build(postorder,inorder,0,postorder.length - 1,0,inorder.length-1);
+        // store inorder indices for O(1) lookup
+        for (int i = 0; i < inorder.length; i++) {
+            inMap.put(inorder[i], i);
+        }
+        return build(postorder, inorder,
+                     0, postorder.length - 1,
+                     0, inorder.length - 1);
     }
-    private TreeNode build(int[] post,int[] ie,int ps,int pe,int is,int in){
-        if(ps > pe || is > in) return null;
-        int rootval = post[pe];
-        TreeNode node = new TreeNode(rootval);
-        int inroot = is;
-        while(ie[inroot] != rootval) inroot++;
-        int leftcount = inroot - is;
-        node.left = build(post,ie,ps,ps+leftcount-1,is,inroot -1);
-        node.right = build(post,ie,ps+leftcount,pe - 1,inroot+1,in);
+
+    private TreeNode build(int[] post, int[] in,
+                           int ps, int pe,
+                           int is, int ie) {       // fix 2: ie not in
+        if (ps > pe || is > ie) return null;
+
+        int rootVal   = post[pe];                  // fix 3: last of postorder
+        TreeNode node = new TreeNode(rootVal);
+
+        int inRoot    = inMap.get(rootVal);         // fix 4: find in inorder
+        int leftCount = inRoot - is;               // fix 5: now correct
+
+        // fix 6: build children and return
+        node.left  = build(post, in,
+                           ps, ps + leftCount - 1,         // left postorder
+                           is, inRoot - 1);                // left inorder
+
+        node.right = build(post, in,
+                           ps + leftCount, pe - 1,         // right postorder
+                           inRoot + 1, ie);                // right inorder
+
         return node;
     }
 }
