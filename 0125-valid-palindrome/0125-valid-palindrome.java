81a869c0acf17a6ae02e0ecd7e877eded5199272
parth7830 <parthkartik125@gmail.com>
2026-07-10 11:29:26 +0530

    Time: 4 ms (48.48%), Space: 44.5 MB (51.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder clean = new StringBuilder();
-        for(int i =0;i<s.length();i++){
+        for(int i = 0;i<s.length();i++){
             char ch = s.charAt(i);
             if(Character.isLetterOrDigit(ch)){
                 clean.append(Character.toLowerCase(ch));
             }
         }
-        int left = 0;int right = clean.length() - 1;
-        while(left<right){
+        int left = 0;
+        int right = clean.length()-1;
+        while(left < right){
             if(clean.charAt(left) != clean.charAt(right)){
                 return false;
             }
@@ -18,3 +19,4 @@ class Solution {
         return true;
     }
 }
+
