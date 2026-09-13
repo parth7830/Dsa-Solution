6bff46345a445f86dd02db9b12160bd7d84cc568
parth7830 <parthkartik125@gmail.com>
2026-09-13 23:30:44 +0530

    Time: 1 ms (13.41%), Space: 42.4 MB (12.82%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,21 +1,15 @@
 class Solution {
-    private static int[][] memo;
-    private static int solver(int i1,int i2,int m, int n,int[][] dirs){
-        if(i1 > m-1 || i2 > n-1) return 0;
-        if(i1 == m-1 && i2 == n-1){
-            return 1;
-        }
-        if(memo[i1][i2] != -1) return memo[i1][i2];
-        int fs = solver(i1 + dirs[0][0],i2 + dirs[0][1],m, n ,dirs);
-        int ss = solver(i1 + dirs[1][0],i2 + dirs[1][1], m, n, dirs);
-        return memo[i1][i2] = fs + ss;
-    }
     public int uniquePaths(int m, int n) {
-        int ind1 = 0;
-        int ind2 = 0;
-        memo = new int[m][n];
-        for (int[] row : memo) Arrays.fill(row, -1);
-        int[][] dirs = {{1,0},{0,1}};
-        return solver(ind1,ind2,m,n,dirs);
+        int[][] dp = new int[m][n];
+        dp[m-1][n-1] = 1;
+        for(int i1 = m-1;i1>=0;i1--){
+            for(int i2 = n-1;i2>=0;i2--){
+                if(i1 == m-1 && i2 == n-1) continue;
+                int fs = i1+1 < m ? dp[i1+1][i2] : 0;
+                int ss = i2+1 < n ? dp[i1][i2+1] : 0;
+                dp[i1][i2] = fs + ss;
+            }
+        }
+        return dp[0][0];
     }
 }
